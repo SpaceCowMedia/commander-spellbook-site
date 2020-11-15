a7f7bc71577d7d6f318e27666121d10635b05080
Blade Barringer <blade@crookedneighbor.com>
2020-11-15 08:31:51 -0600

    Add Placedholder component

---
+++ test/components/PlaceholderText.spec.js
@@ -0,0 +1,11 @@
+import { shallowMount } from '@vue/test-utils'
+import PlaceholderText from '@/components/PlaceholderText.vue'
+
+describe('PlaceholderText', () => {
+  test('creates a text-bar element with a set width', () => {
+    const wrapper = shallowMount(PlaceholderText)
+
+    expect(wrapper.find('.text-bar').element.style).toBeTruthy()
+    expect(wrapper.find('.text-bar').attributes('style')).toMatch(/width: \d*%/)
+  })
+})
