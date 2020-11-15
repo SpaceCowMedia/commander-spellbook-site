636d2f7e10f89dddfa19530c0e18c02db3fcd030
Blade Barringer <blade@crookedneighbor.com>
2020-11-15 12:49:38 -0600

    Add combo sidebar links component

---
+++ components/combo/ComboSidebarLinks.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="mt-4 mb-4 w-full rounded overflow-hidden">
+    <button
+      class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+    >
+      Copy Combo Link
+    </button>
+
+    <br />
+    <br />
+    <p>Additional utilities here</p>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({})
+</script>
