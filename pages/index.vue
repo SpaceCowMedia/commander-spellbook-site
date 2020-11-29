d581c8fa85d42118ff654357daf419253573c98f
Blade Barringer <blade@crookedneighbor.com>
2020-11-29 08:50:08 -0600

    Update landing page to automatically redirect based on query

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,9 @@
     <div class="container">
       <div class="w-full">
         <Logo />
+
         <SearchBar class="md:w-2/3 h-20" inputClass="text-2xl text-center" />
+
         <div class="links">
           <nuxt-link to="/advanced-search" class="button--red">
             Advanced Search
@@ -21,10 +23,35 @@
 <script lang="ts">
 import Vue from 'vue'
 
-export default Vue.extend({})
+export default Vue.extend({
+  mounted() {
+    const query = this.$route.query.q
+
+    if (!(typeof query === 'string')) {
+      return
+    }
+
+    if (Number(query) > 0) {
+      this.$router.push(`/combo/${query}`)
+      return
+    }
+
+    if (query === 'spoiled') {
+      this.$router.push('/search?q=is:spoiled')
+      return
+    }
+
+    if (query === 'banned') {
+      this.$router.push('/search?q=is:banned')
+      return
+    }
+
+    this.$router.push(`/search?q=${query}`)
+  },
+})
 </script>
 
-<style>
+<style scoped>
 /* Sample `apply` at-rules with Tailwind CSS
 .container {
 @apply min-h-screen flex justify-center items-center text-center mx-auto;
@@ -34,9 +61,5 @@ export default Vue.extend({})
   margin: 0 auto;
   min-height: 100vh;
   @apply flex flex-col items-center justify-center text-center;
-}
-
-.links {
-  padding-top: 15px;
 }
 </style>
---
+++ test/pages/index.spec.js
@@ -0,0 +1,71 @@
+import { shallowMount } from '@vue/test-utils'
+import HomePage from '@/pages/index.vue'
+
+describe('HomePage', () => {
+  let $route, $router, wrapperOptions
+
+  beforeEach(() => {
+    $route = {
+      query: {},
+    }
+    $router = {
+      push: jest.fn(),
+    }
+    wrapperOptions = {
+      mocks: {
+        $route,
+        $router,
+      },
+      stubs: {
+        SearchBar: true,
+        Logo: true,
+        NuxtLink: true,
+      },
+    }
+  })
+
+  it('remains on home page when no query is available', () => {
+    shallowMount(HomePage, wrapperOptions)
+
+    expect($router.push).not.toBeCalled()
+  })
+
+  it('remains on home page when query is not a string', () => {
+    $route.query.q = ['foo', 'bar']
+    shallowMount(HomePage, wrapperOptions)
+
+    expect($router.push).not.toBeCalled()
+  })
+
+  it('redirects to search page when query is available', () => {
+    $route.query.q = 'card:Sydri'
+    shallowMount(HomePage, wrapperOptions)
+
+    expect($router.push).toBeCalledTimes(1)
+    expect($router.push).toBeCalledWith('/search?q=card:Sydri')
+  })
+
+  it('redirects to combo page when query is a number', () => {
+    $route.query.q = '435'
+    shallowMount(HomePage, wrapperOptions)
+
+    expect($router.push).toBeCalledTimes(1)
+    expect($router.push).toBeCalledWith('/combo/435')
+  })
+
+  it('redirects to search results page when query is a spoiled', () => {
+    $route.query.q = 'spoiled'
+    shallowMount(HomePage, wrapperOptions)
+
+    expect($router.push).toBeCalledTimes(1)
+    expect($router.push).toBeCalledWith('/search?q=is:spoiled')
+  })
+
+  it('redirects to search results page when query is a banned', () => {
+    $route.query.q = 'banned'
+    shallowMount(HomePage, wrapperOptions)
+
+    expect($router.push).toBeCalledTimes(1)
+    expect($router.push).toBeCalledWith('/search?q=is:banned')
+  })
+})
