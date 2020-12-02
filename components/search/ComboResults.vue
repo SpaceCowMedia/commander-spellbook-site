145894a0d73d5e4b33626d182291cd9ac54cf4e4
Blade Barringer <blade@crookedneighbor.com>
2020-12-01 22:53:57 -0600

    Update combo result component

--- components/search/ComboResults.vue
+++ components/search/ComboResults.vue
@@ -1,8 +1,38 @@
 <template>
-  <div class="combo-results">
-    <div v-for="r in results" :key="'combo-' + r.id" class="combo-result">
-      <nuxt-link :to="'/combo/' + r.id">{{ r.names }}</nuxt-link>
-    </div>
+  <div class="combo-results-wrapper">
+    <nuxt-link
+      v-for="r in results"
+      :key="'combo_' + r.id"
+      :to="'/combo/' + r.id"
+      class="combo-result w-full sm:w-1/3"
+    >
+      <div class="flex border-b-2 border-gray-300">
+        <div class="flex-grow w-1/2">
+          <div class="pl-3 pr-3 p-1 bg-gray-300 mb-2">
+            <strong>Cards</strong>
+          </div>
+          <CardTooltip
+            v-for="name in r.names"
+            :cardName="name"
+            :key="r.id + '_' + name"
+          >
+            <div class="card-name pl-3 pr-3">{{ name }}</div>
+          </CardTooltip>
+        </div>
+        <div class="border-l-2 border-gray-300 flex-grow w-1/2">
+          <div class="pl-3 pr-3 p-1 bg-gray-300 mb-2">
+            <strong>Results</strong>
+          </div>
+          <div v-for="result in r.results" class="result pl-3 pr-3">
+            {{ result }}
+          </div>
+        </div>
+      </div>
+
+      <div class="flex items-center h-full">
+        <ColorIdentity :colors="r.colors" />
+      </div>
+    </nuxt-link>
   </div>
 </template>
 
@@ -11,7 +41,9 @@ import Vue, { PropType } from 'vue'
 
 export type ComboResult = {
   id: string
-  names: string
+  names: string[]
+  results: string[]
+  colors: string[]
 }
 
 export default Vue.extend({
@@ -27,5 +59,11 @@ export default Vue.extend({
 </script>
 
 <style scoped>
-/* todo */
+.combo-results-wrapper {
+  @apply flex flex-wrap;
+}
+
+.combo-result {
+  @apply block m-4 rounded border-2 border-gray-300 flex-grow flex flex-col content-center;
+}
 </style>
--- test/components/search/ComboResults.spec.js
+++ test/components/search/ComboResults.spec.js
@@ -2,31 +2,144 @@ import { shallowMount, RouterLinkStub } from '@vue/test-utils'
 import ComboResults from '@/components/search/ComboResults.vue'
 
 describe('ComboResults', () => {
-  test('creates a link for each combo result', () => {
-    const wrapper = shallowMount(ComboResults, {
+  let options
+
+  beforeEach(() => {
+    options = {
       stubs: {
         NuxtLink: RouterLinkStub,
+        ColorIdentity: true,
+        CardTooltip: true,
       },
       propsData: {
         results: [
           {
-            names: 'a, b, c',
+            names: ['a', 'b', 'c'],
             id: '1',
+            results: ['result 1', 'result 2'],
+            colors: ['w', 'b', 'r'],
           },
           {
-            names: 'd, e, f',
+            names: ['d', 'e', 'f', 'g'],
             id: '2',
+            results: ['result 3', 'result 4'],
+            colors: ['g', 'u'],
           },
           {
-            names: 'g, h, i',
+            names: ['h', 'i'],
             id: '3',
+            results: ['result 5', 'result 6'],
+            colors: ['c'],
           },
         ],
       },
-    })
+    }
+  })
+
+  test('creates a link for each combo result', () => {
+    const wrapper = shallowMount(ComboResults, options)
 
     const links = wrapper.findAllComponents(RouterLinkStub)
 
     expect(links.length).toBe(3)
+    expect(links.at(0).props('to')).toBe('/combo/1')
+    expect(links.at(1).props('to')).toBe('/combo/2')
+    expect(links.at(2).props('to')).toBe('/combo/3')
+  })
+
+  test('creates a card tooltip for each name', () => {
+    const CardTooltipStub = {
+      template: '<div><slot /></div>',
+    }
+    options.stubs.CardTooltip = CardTooltipStub
+    const wrapper = shallowMount(ComboResults, options)
+
+    const links = wrapper.findAllComponents(RouterLinkStub)
+    const firstCardTooltips = links.at(0).findAllComponents(CardTooltipStub)
+    const secondCardTooltips = links.at(1).findAllComponents(CardTooltipStub)
+    const thirdCardTooltips = links.at(2).findAllComponents(CardTooltipStub)
+
+    expect(firstCardTooltips.length).toBe(3)
+    expect(firstCardTooltips.at(0).find('.card-name').element.textContent).toBe(
+      'a'
+    )
+    expect(firstCardTooltips.at(1).find('.card-name').element.textContent).toBe(
+      'b'
+    )
+    expect(firstCardTooltips.at(2).find('.card-name').element.textContent).toBe(
+      'c'
+    )
+    expect(secondCardTooltips.length).toBe(4)
+    expect(
+      secondCardTooltips.at(0).find('.card-name').element.textContent
+    ).toBe('d')
+    expect(
+      secondCardTooltips.at(1).find('.card-name').element.textContent
+    ).toBe('e')
+    expect(
+      secondCardTooltips.at(2).find('.card-name').element.textContent
+    ).toBe('f')
+    expect(
+      secondCardTooltips.at(3).find('.card-name').element.textContent
+    ).toBe('g')
+    expect(thirdCardTooltips.length).toBe(2)
+    expect(thirdCardTooltips.at(0).find('.card-name').element.textContent).toBe(
+      'h'
+    )
+    expect(thirdCardTooltips.at(1).find('.card-name').element.textContent).toBe(
+      'i'
+    )
+  })
+
+  test('prints results for each combo', () => {
+    const wrapper = shallowMount(ComboResults, options)
+
+    const links = wrapper.findAllComponents(RouterLinkStub)
+
+    expect(links.at(0).findAll('.result').at(0).element.textContent).toContain(
+      'result 1'
+    )
+    expect(links.at(0).findAll('.result').at(1).element.textContent).toContain(
+      'result 2'
+    )
+    expect(links.at(1).findAll('.result').at(0).element.textContent).toContain(
+      'result 3'
+    )
+    expect(links.at(1).findAll('.result').at(1).element.textContent).toContain(
+      'result 4'
+    )
+    expect(links.at(2).findAll('.result').at(0).element.textContent).toContain(
+      'result 5'
+    )
+    expect(links.at(2).findAll('.result').at(1).element.textContent).toContain(
+      'result 6'
+    )
+  })
+
+  test('prints the color identity', () => {
+    const CIStub = {
+      template: '<div></div>',
+      props: {
+        colors: {
+          type: Array,
+          default: [],
+        },
+      },
+    }
+    options.stubs.ColorIdentity = CIStub
+    const wrapper = shallowMount(ComboResults, options)
+
+    const links = wrapper.findAllComponents(RouterLinkStub)
+
+    expect(links.at(0).findComponent(CIStub).props('colors')).toEqual([
+      'w',
+      'b',
+      'r',
+    ])
+    expect(links.at(1).findComponent(CIStub).props('colors')).toEqual([
+      'g',
+      'u',
+    ])
+    expect(links.at(2).findComponent(CIStub).props('colors')).toEqual(['c'])
   })
 })
