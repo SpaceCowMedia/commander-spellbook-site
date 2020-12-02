da363a45e623b420274176043206efdd46a71c7d
Blade Barringer <blade@crookedneighbor.com>
2020-12-02 09:36:19 -0600

    Add card tooltip component

---
+++ components/CardTooltip.vue
@@ -0,0 +1,62 @@
+<template>
+  <span @mousemove="mousemove" @mouseout="mouseout">
+    <div v-if="hover" class="card-tooltip" :style="{ left, top }">
+      <img v-if="cardName" :src="imgSrc" />
+    </div>
+    <slot />
+  </span>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import scryfall from 'scryfall-client'
+
+type TooltipData = {
+  hover: boolean
+  left: string
+  top: string
+}
+
+export default Vue.extend({
+  data(): TooltipData {
+    return {
+      hover: false,
+      left: '0px',
+      top: '0px',
+    }
+  },
+  props: {
+    cardName: {
+      type: String,
+      default: '',
+    },
+  },
+  methods: {
+    mousemove(event: MouseEvent): void {
+      this.hover = true
+      this.left = event.clientX + 50 + 'px'
+      this.top = event.clientY - 30 + 'px'
+    },
+    mouseout(): void {
+      this.hover = false
+    },
+  },
+  computed: {
+    imgSrc(): string {
+      return `https://api.scryfall.com/cards/named?exact=${this.cardName}&format=image`
+    },
+  },
+})
+</script>
+
+<style scoped>
+.card-tooltip {
+  pointer-events: none;
+  position: fixed;
+  z-index: 9000000;
+  border-radius: 4.75% / 3.5%;
+  height: 340px;
+  width: 244px;
+  overflow: hidden;
+}
+</style>
---
+++ test/components/CardTooltip.spec.js
@@ -0,0 +1,47 @@
+import { shallowMount } from '@vue/test-utils'
+import CardTooltip from '@/components/CardTooltip.vue'
+
+describe('CardTooltip', () => {
+  let options
+
+  beforeEach(() => {
+    options = {
+      propsData: {
+        cardName: 'Sydri',
+      },
+      slots: {
+        default: '<div></div>',
+      },
+    }
+  })
+
+  test('is hidden by default', () => {
+    const wrapper = shallowMount(CardTooltip, options)
+
+    expect(wrapper.find('.card-tooltip').exists()).toBe(false)
+  })
+
+  test('reveals and hides tooltip on mousemove and mouseout', async () => {
+    const wrapper = shallowMount(CardTooltip, options)
+
+    await wrapper.find('span').trigger('mousemove')
+    expect(wrapper.find('.card-tooltip').exists()).toBe(true)
+    expect(wrapper.find('.card-tooltip img').element.src).toBe(
+      'https://api.scryfall.com/cards/named?exact=Sydri&format=image'
+    )
+
+    await wrapper.find('span').trigger('mouseout')
+    expect(wrapper.find('.card-tooltip').exists()).toBe(false)
+  })
+
+  test('sets the tooltip position based on the mousemove event', async () => {
+    const wrapper = shallowMount(CardTooltip, options)
+
+    await wrapper.find('span').trigger('mousemove', {
+      clientX: 23,
+      clientY: 12,
+    })
+    expect(wrapper.find('.card-tooltip').element.style.left).toBe('73px')
+    expect(wrapper.find('.card-tooltip').element.style.top).toBe('-18px')
+  })
+})
