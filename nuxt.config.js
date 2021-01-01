4b574cce6bea6de701f315466db80a88aae06c8c
Blade Barringer <blade@crookedneighbor.com>
2020-12-31 23:06:04 -0600

    chore: add 404 page generation

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,6 +16,8 @@ export default {
   // Global CSS (https://go.nuxtjs.dev/config-css)
   css: [],
 
+  generate: { fallback: "404.html" },
+
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [],
 
