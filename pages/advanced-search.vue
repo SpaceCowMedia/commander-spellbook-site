13f3eabd08c84b496463ffbc7aee842bd291c549
Blade Barringer <blade@crookedneighbor.com>
2021-01-03 16:24:02 -0600

    feat: add first draft of advanced search page

--- pages/advanced-search.vue
+++ pages/advanced-search.vue
@@ -1,17 +1,229 @@
 <template>
   <div>
+    <SearchBar />
+
     <div class="container">
-      <div class="w-full">Advanced Search Stuff</div>
+      <h2 class="heading-title">Advanced Search</h2>
+      <p>TODO: Intro text about how this works</p>
     </div>
+
+    <form @submit.prevent="submit">
+      <div class="divider"></div>
+
+      <div class="container">
+        <MultiSearchInput
+          label="Card Name"
+          placeholder="Card Name"
+          @update="updateCards"
+        />
+      </div>
+
+      <div class="divider"></div>
+
+      <div class="container">
+        <div class="my-2 flex">
+          <div class="w-1/3 flex-grow">
+            <span>Color Identity</span>
+          </div>
+          <div class="w-2/3 flex flex-row">
+            <div
+              v-for="(color, index) in colorIdentity"
+              :key="`ci-input-${index}`"
+              class="color-identity-wrapper cursor-pointer"
+              :class="{ 'opacity-25': !color.checked }"
+              @click="toggleColorIdentity(index)"
+            >
+              <ColorIdentity :colors="[color.symbol]" />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="divider"></div>
+
+      <div class="container">
+        <MultiSearchInput
+          label="Prerequisite"
+          placeholder="Text"
+          @update="updatePrerequisites"
+        />
+      </div>
+
+      <div class="divider"></div>
+
+      <div class="container">
+        <MultiSearchInput
+          label="Step"
+          placeholder="Text"
+          @update="updateSteps"
+        />
+      </div>
+
+      <div class="divider"></div>
+
+      <div class="container">
+        <MultiSearchInput
+          label="Result"
+          placeholder="Text"
+          @update="updateResults"
+        />
+      </div>
+
+      <div class="divider"></div>
+
+      <div class="container">
+        <button type="submit" class="button--red">Search</button>
+      </div>
+    </form>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 
-export default Vue.extend({});
+type ColorIdentityInput = {
+  symbol: string;
+  checked: boolean;
+};
+
+type Data = {
+  cards: string[];
+  prerequisites: string[];
+  steps: string[];
+  results: string[];
+  colorIdentity: ColorIdentityInput[];
+};
+
+export default Vue.extend({
+  data(): Data {
+    return {
+      cards: [],
+      prerequisites: [],
+      steps: [],
+      results: [],
+      colorIdentity: [
+        {
+          symbol: "w",
+          checked: true,
+        },
+        {
+          symbol: "u",
+          checked: true,
+        },
+        {
+          symbol: "b",
+          checked: true,
+        },
+        {
+          symbol: "r",
+          checked: true,
+        },
+        {
+          symbol: "g",
+          checked: true,
+        },
+      ],
+    };
+  },
+  methods: {
+    submit(): void {
+      let query = "";
+
+      function makeQueryFunction(
+        key: string
+      ): Parameters<typeof Array.prototype.forEach>[0] {
+        return (value: string) => {
+          if (!value.trim()) {
+            return;
+          }
+
+          let quotes = '"';
+
+          if (value.includes(quotes)) {
+            if (value.includes("'")) {
+              // malformed if it includes both double quotes and single quotes
+              return;
+            }
+            quotes = "'";
+          }
+
+          query += ` ${key}:${quotes}${value}${quotes}`;
+        };
+      }
+
+      this.cards.forEach(makeQueryFunction("card"));
+
+      const colors = this.colorIdentity.reduce((accum, color) => {
+        if (color.checked) {
+          accum += color.symbol;
+        }
+
+        return accum;
+      }, "");
+
+      if (!colors) {
+        query += " ci:colorless";
+      } else if (colors.length < 5) {
+        query += ` ci:${colors}`;
+      }
+
+      this.prerequisites.forEach(makeQueryFunction("pre"));
+      this.steps.forEach(makeQueryFunction("step"));
+      this.results.forEach(makeQueryFunction("result"));
+
+      query = query.trim();
+
+      if (!query) {
+        // TODO erorr
+        return;
+      }
+
+      this.$router.push({
+        path: "/search",
+        query: {
+          q: `${query}`,
+        },
+      });
+    },
+    toggleColorIdentity(index: number): void {
+      this.colorIdentity[index].checked = !this.colorIdentity[index].checked;
+    },
+    updateCards(payload: { index: number; value: string }): void {
+      this.cards[payload.index] = payload.value;
+    },
+    updatePrerequisites(payload: { index: number; value: string }): void {
+      this.prerequisites[payload.index] = payload.value;
+    },
+    updateSteps(payload: { index: number; value: string }): void {
+      this.steps[payload.index] = payload.value;
+    },
+    updateResults(payload: { index: number; value: string }): void {
+      this.results[payload.index] = payload.value;
+    },
+  },
+});
 </script>
 
 <style>
-/* styles here */
+.divider {
+  border-color: rgb(92, 126, 159);
+  @apply border-t w-full;
+}
+.container {
+  @apply max-w-5xl m-auto mt-6 mb-6 px-4;
+}
+
+.subtitle {
+  @apply font-light text-3xl;
+}
+
+.input {
+  border-color: rgb(92, 126, 159);
+  @apply appearance-none block bg-gray-200 text-gray-700 border py-2 px-4 leading-tight m-auto w-full max-w-5xl;
+}
+
+.input-button {
+  background: rgb(92, 126, 159);
+  @apply px-2 text-white flex flex-row items-center text-3xl;
+}
 </style>
---
+++ test/pages/advanced-search.spec.ts
@@ -0,0 +1,377 @@
+import { shallowMount } from "@vue/test-utils";
+import AdvancedSearchPage from "@/pages/advanced-search.vue";
+
+import type { Router, VueComponent } from "../types";
+
+describe("AdvancedSearchPage", () => {
+  let $router: Router;
+
+  beforeEach(() => {
+    $router = {
+      push: jest.fn(),
+    };
+  });
+
+  describe("submit", () => {
+    it("redirects to search with query based on data", () => {
+      const wrapper = shallowMount(AdvancedSearchPage, {
+        mocks: {
+          $router,
+        },
+        stubs: {
+          SearchBar: true,
+          ColorIdentity: true,
+          MultiSearchInput: true,
+        },
+      });
+
+      wrapper.setData({
+        cards: ["card 1", "card 2"],
+        prerequisites: ["pre 1", "pre 2"],
+        steps: ["step 1", "step 2"],
+        results: ["result 1", "result 2"],
+        colorIdentity: [
+          {
+            symbol: "w",
+            checked: true,
+          },
+          {
+            symbol: "u",
+            checked: false,
+          },
+        ],
+      });
+
+      (wrapper.vm as VueComponent).submit();
+
+      expect($router.push).toBeCalledTimes(1);
+      expect($router.push).toBeCalledWith({
+        path: "/search",
+        query: {
+          q: `card:"card 1" card:"card 2" ci:w pre:"pre 1" pre:"pre 2" step:"step 1" step:"step 2" result:"result 1" result:"result 2"`,
+        },
+      });
+    });
+
+    it("does not redirect when query is empty", () => {
+      const wrapper = shallowMount(AdvancedSearchPage, {
+        mocks: {
+          $router,
+        },
+        stubs: {
+          SearchBar: true,
+          ColorIdentity: true,
+          MultiSearchInput: true,
+        },
+      });
+
+      (wrapper.vm as VueComponent).submit();
+
+      expect($router.push).not.toBeCalled();
+    });
+
+    it("uses coloreless identity when no colors are checked", () => {
+      const wrapper = shallowMount(AdvancedSearchPage, {
+        mocks: {
+          $router,
+        },
+        stubs: {
+          SearchBar: true,
+          ColorIdentity: true,
+          MultiSearchInput: true,
+        },
+      });
+      wrapper.setData({
+        colorIdentity: [
+          {
+            symbol: "w",
+            checked: false,
+          },
+          {
+            symbol: "u",
+            checked: false,
+          },
+          {
+            symbol: "b",
+            checked: false,
+          },
+          {
+            symbol: "r",
+            checked: false,
+          },
+          {
+            symbol: "g",
+            checked: false,
+          },
+        ],
+      });
+
+      (wrapper.vm as VueComponent).submit();
+
+      expect($router.push).toBeCalledTimes(1);
+      expect($router.push).toBeCalledWith({
+        path: "/search",
+        query: {
+          q: "ci:colorless",
+        },
+      });
+    });
+
+    it("does not use color identity if all colores are checked", () => {
+      const wrapper = shallowMount(AdvancedSearchPage, {
+        mocks: {
+          $router,
+        },
+        stubs: {
+          SearchBar: true,
+          ColorIdentity: true,
+          MultiSearchInput: true,
+        },
+      });
+      wrapper.setData({
+        cards: ["card 1"],
+        colorIdentity: [
+          {
+            symbol: "w",
+            checked: true,
+          },
+          {
+            symbol: "u",
+            checked: true,
+          },
+          {
+            symbol: "b",
+            checked: true,
+          },
+          {
+            symbol: "r",
+            checked: true,
+          },
+          {
+            symbol: "g",
+            checked: true,
+          },
+        ],
+      });
+
+      (wrapper.vm as VueComponent).submit();
+
+      expect($router.push).toBeCalledTimes(1);
+      expect($router.push).toBeCalledWith({
+        path: "/search",
+        query: {
+          q: `card:"card 1"`,
+        },
+      });
+    });
+
+    it("ignores data with empty spaces", () => {
+      const wrapper = shallowMount(AdvancedSearchPage, {
+        mocks: {
+          $router,
+        },
+        stubs: {
+          SearchBar: true,
+          ColorIdentity: true,
+          MultiSearchInput: true,
+        },
+      });
+      wrapper.setData({
+        cards: ["card 1", "", "        ", "card 2"],
+      });
+
+      (wrapper.vm as VueComponent).submit();
+
+      expect($router.push).toBeCalledTimes(1);
+      expect($router.push).toBeCalledWith({
+        path: "/search",
+        query: {
+          q: `card:"card 1" card:"card 2"`,
+        },
+      });
+    });
+
+    it("uses single quotes when data includes double quotes", () => {
+      const wrapper = shallowMount(AdvancedSearchPage, {
+        mocks: {
+          $router,
+        },
+        stubs: {
+          SearchBar: true,
+          ColorIdentity: true,
+          MultiSearchInput: true,
+        },
+      });
+      wrapper.setData({
+        cards: ['Card with "symbols"'],
+      });
+
+      (wrapper.vm as VueComponent).submit();
+
+      expect($router.push).toBeCalledTimes(1);
+      expect($router.push).toBeCalledWith({
+        path: "/search",
+        query: {
+          q: `card:'Card with "symbols"'`,
+        },
+      });
+    });
+
+    it("ignores query if it contains both single and double quotes", () => {
+      const wrapper = shallowMount(AdvancedSearchPage, {
+        mocks: {
+          $router,
+        },
+        stubs: {
+          SearchBar: true,
+          ColorIdentity: true,
+          MultiSearchInput: true,
+        },
+      });
+      wrapper.setData({
+        cards: [`Card with "symbols" and 'symbols'`, "basic card"],
+      });
+
+      (wrapper.vm as VueComponent).submit();
+
+      expect($router.push).toBeCalledTimes(1);
+      expect($router.push).toBeCalledWith({
+        path: "/search",
+        query: {
+          q: `card:"basic card"`,
+        },
+      });
+    });
+  });
+
+  describe("toggleColorIdentity", () => {
+    it("toggles the state of the color identity when clicked", async () => {
+      const wrapper = shallowMount(AdvancedSearchPage, {
+        stubs: {
+          SearchBar: true,
+          ColorIdentity: true,
+          MultiSearchInput: true,
+        },
+      });
+      const vm = wrapper.vm as VueComponent;
+
+      expect(vm.colorIdentity[0].checked).toBe(true);
+      await wrapper.findAll(".color-identity-wrapper").at(0).trigger("click");
+      expect(vm.colorIdentity[0].checked).toBe(false);
+      await wrapper.findAll(".color-identity-wrapper").at(0).trigger("click");
+      expect(vm.colorIdentity[0].checked).toBe(true);
+
+      expect(vm.colorIdentity[2].checked).toBe(true);
+      await wrapper.findAll(".color-identity-wrapper").at(2).trigger("click");
+      expect(vm.colorIdentity[2].checked).toBe(false);
+    });
+  });
+
+  describe("updateCards", () => {
+    it("is a listener on a multi search input", async () => {
+      const FakeMultiSearchInput = {
+        template: "<div></div>",
+        props: ["label", "placeholder"],
+      };
+      const wrapper = shallowMount(AdvancedSearchPage, {
+        stubs: {
+          SearchBar: true,
+          ColorIdentity: true,
+          MultiSearchInput: FakeMultiSearchInput,
+        },
+      });
+
+      const cardsInput = wrapper.findAllComponents(FakeMultiSearchInput).at(0);
+
+      expect(cardsInput.props("label")).toBe("Card Name");
+
+      await cardsInput.vm.$emit("update", {
+        index: 1,
+        value: "new card",
+      });
+
+      expect((wrapper.vm as VueComponent).cards[1]).toBe("new card");
+    });
+  });
+
+  describe("updatePrerequisites", () => {
+    it("is a listener on a multi search input", async () => {
+      const FakeMultiSearchInput = {
+        template: "<div></div>",
+        props: ["label", "placeholder"],
+      };
+      const wrapper = shallowMount(AdvancedSearchPage, {
+        stubs: {
+          SearchBar: true,
+          ColorIdentity: true,
+          MultiSearchInput: FakeMultiSearchInput,
+        },
+      });
+
+      const preReqInput = wrapper.findAllComponents(FakeMultiSearchInput).at(1);
+
+      expect(preReqInput.props("label")).toBe("Prerequisite");
+
+      await preReqInput.vm.$emit("update", {
+        index: 1,
+        value: "new prereq",
+      });
+
+      expect((wrapper.vm as VueComponent).prerequisites[1]).toBe("new prereq");
+    });
+  });
+
+  describe("updateSteps", () => {
+    it("is a listener on a multi search input", async () => {
+      const FakeMultiSearchInput = {
+        template: "<div></div>",
+        props: ["label", "placeholder"],
+      };
+      const wrapper = shallowMount(AdvancedSearchPage, {
+        stubs: {
+          SearchBar: true,
+          ColorIdentity: true,
+          MultiSearchInput: FakeMultiSearchInput,
+        },
+      });
+
+      const stepInput = wrapper.findAllComponents(FakeMultiSearchInput).at(2);
+
+      expect(stepInput.props("label")).toBe("Step");
+
+      await stepInput.vm.$emit("update", {
+        index: 1,
+        value: "new step",
+      });
+
+      expect((wrapper.vm as VueComponent).steps[1]).toBe("new step");
+    });
+  });
+
+  describe("updateResults", () => {
+    it("is a listener on a multi search input", async () => {
+      const FakeMultiSearchInput = {
+        template: "<div></div>",
+        props: ["label", "placeholder"],
+      };
+      const wrapper = shallowMount(AdvancedSearchPage, {
+        stubs: {
+          SearchBar: true,
+          ColorIdentity: true,
+          MultiSearchInput: FakeMultiSearchInput,
+        },
+      });
+
+      const resultInput = wrapper.findAllComponents(FakeMultiSearchInput).at(3);
+
+      expect(resultInput.props("label")).toBe("Result");
+
+      await resultInput.vm.$emit("update", {
+        index: 1,
+        value: "new result",
+      });
+
+      expect((wrapper.vm as VueComponent).results[1]).toBe("new result");
+    });
+  });
+});
