cb28bf7f729d9bfd39264abee534d1ffbfd962bc
Blade Barringer <blade@crookedneighbor.com>
2021-01-07 22:23:09 -0600

    fix: revert accidental commit of combo index page

--- pages/combo/index.vue
+++ pages/combo/index.vue
@@ -4,10 +4,10 @@
       this page mostly exists to statically render pages for each combo in the `npm run generate` call
       if this pages is actually exposed on the site via links,
       it should probably get some major design work :)
+    -->
     <p v-for="link in links" :key="link.id">
       <nuxt-link :to="'/combo/' + link.id">{{ link.names }}</nuxt-link>
     </p>
-    -->
   </div>
 </template>
 
