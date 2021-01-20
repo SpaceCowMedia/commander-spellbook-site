51d1569063635d89ee0c7a1cd820771121782fd4
Connor Phelan <Connor.V.Phelan@gmail.com>
2021-01-20 15:40:30 -0500

    feat: initial commit

---
+++ pages/about.vue
@@ -0,0 +1,45 @@
+// code copied directly from v1 site, stlying TBD
+// all modal formatting needs to be removed and reworked into static page
+
+  <!-- About Modal -->
+  <div class="modal fade" id="aboutModal" tabindex="-1" role="dialog" aria-labelledby="aboutLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="aboutLabel">About</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          The Commander Spellbook project exists to catalog all reasonable Commander/EDH combos and to make them easily
+          available across all modern digital platforms. This community driven project is used to power <a
+            href="https://edhrec.com/combos" target="_blank">EDHREC's Combo Feature</a>.
+          <p>
+            <p>The database and the source code for the website are <a href="https://opensource.org/licenses/MIT"
+                target="_blank">completely free and open
+                source under the MIT License</a>. We encourage you to copy this data so that it lives on!</p>
+          </p>
+          <ul>
+            <li><a target="_blank" href="https://github.com/Commander-Spellbook/website-v1">Website Source Code on
+                GitHub</a></li>
+            <li><a target="_blank"
+                href="https://docs.google.com/spreadsheets/d/1JJo8MzkpuhfvsaKVFVlOoNymscCt-Aw-1sob2IhpwXY/">Combo
+                Database Backend on Google Sheets</a></li>
+            <li><a target="_blank"
+                href="https://sheets.googleapis.com/v4/spreadsheets/1JJo8MzkpuhfvsaKVFVlOoNymscCt-Aw-1sob2IhpwXY/values:batchGet?ranges=combos!A2:R&key=AIzaSyDzQ0jCf3teHnUK17ubaLaV6rcWf9ZjG5E">Combo
+                Database GET API on Google Sheets</a></li>
+          </ul>
+          <strong>Sincerely, the Community Admins,</strong>
+          <br>- <a href="https://www.moxfield.com/users/SeniorEdificer" target="_blank">Senior Edificer</a>
+          <br>- <a href="https://archidekt.com/user/17861" target="_blank">Goldshot20</a>
+          <br>- <a href="https://www.moxfield.com/users/AppleSaws" target="_blank">AppleSaws</a>
+          <br>- <a href="https://tappedout.net/users/LapperMedic/" target="_blank">Lapper</a>
+          <hr>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+    </div>
+  </div>
---
+++ pages/metrics.vue
@@ -0,0 +1,38 @@
+// code copied directly from v1 site, styling TBD
+// all modal formatting needs to be removed and reworked into static page
+
+ <!-- Metrics Modal -->
+  <div class="modal fade" id="metricsModal" tabindex="-1" role="dialog" aria-labelledby="metricsLabel"
+    aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="metricsLabel">Combo Metrics</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <table class="table">
+            <thead>
+              <tr>
+                <th width="38%">
+                  <center>METRIC</center>
+                </th>
+                <th width="57%">
+                  <center>IDENTITY</center>
+                </th>
+                <th width="5%">
+                  <center>COUNT</center>
+                </th>
+              </tr>
+            </thead>
+            <tbody id="metrics"></tbody>
+          </table>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+    </div>
+  </div>
