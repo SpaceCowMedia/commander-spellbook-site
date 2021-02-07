03f4fcad013b7b6a32305b5614dddfbaba851fc4
Blade Barringer <blade@crookedneighbor.com>
2021-02-07 10:47:11 -0600

    fix: add html lang attribute to nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,6 +5,9 @@ export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     title: "Commander Spellbook",
+    htmlAttrs: {
+      lang: "en",
+    },
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
