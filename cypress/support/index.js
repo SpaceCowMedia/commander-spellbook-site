05dcc4bd443000675ae535536b0451887873e86f
Blade Barringer <blade@crookedneighbor.com>
2021-02-11 07:46:46 -0600

    chore: update integration test helper to fixup analytics cookies

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,9 +1,12 @@
-beforeAll(() => {
+beforeEach(() => {
   cy.visit("/");
+
+  // deny the cookies so we don't run google analytics tracking
+  // during integration tests
+  cy.clearLocalStorage().then((ls) => {
+    ls.setItem("GDPR:accepted", "false");
+  });
 
   // TODO remove this when the beta mask component is removed
   cy.get("input#beta-password").type("arjun{enter}");
-  // deny the cookies so we don't run google analytics tracking
-  // during integration tests
-  cy.get("#cookie-deny-button").click();
 });
