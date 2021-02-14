503bc33d40ca056f7cc8270ddaf2e37a793b6256
Blade Barringer <blade@crookedneighbor.com>
2021-02-14 13:34:25 -0600

    feat: add radio search input component

---
+++ components/advanced-search/RadioSearchInput.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <div class="font-semibold">{{ label }}</div>
+
+    <div class="flex flex-col">
+      <label
+        v-for="(option, index) in options"
+        :key="`${label}-radio-input-${index}`"
+        class="radio-wrapper sm:inline-flex items-center mt-3"
+      >
+        <input
+          type="radio"
+          :name="formName"
+          class="h-5 w-5"
+          :checked="checkedValue === option.value"
+          :value="option.value"
+          @change="updateRadio(option.value)"
+        /><span class="ml-2 text-dark">{{ option.label }}</span>
+      </label>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue, { PropType } from "vue";
+
+export default Vue.extend({
+  props: {
+    checkedValue: {
+      type: String,
+      default: "",
+    },
+    options: {
+      type: Array as PropType<{ value: string; label: string }[]>,
+      default() {
+        return [];
+      },
+    },
+    formName: {
+      type: String,
+      default: "",
+    },
+    label: {
+      type: String,
+      default: "",
+    },
+  },
+  methods: {
+    updateRadio(value: string) {
+      this.$emit("update-radio", value);
+    },
+  },
+});
+</script>
