64061e8d7634aa9d43481504a3ae3560d11c51d5
Blade Barringer <blade@crookedneighbor.com>
2021-03-01 21:15:32 -0600

    feat: create card link component

---
+++ components/CardLink.vue
@@ -0,0 +1,33 @@
+<template>
+  <a :href="link" :target="target"><slot /></a>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  props: {
+    name: {
+      type: String,
+      default: "",
+    },
+    target: {
+      type: String,
+      default: "_blank",
+    },
+  },
+  computed: {
+    link(): string {
+      let quotes = "%22";
+
+      if (this.name.includes('"')) {
+        quotes = "%27";
+      }
+
+      return `https://scryfall.com/search?q=name%3D${quotes}${encodeURIComponent(
+        this.name
+      )}${quotes}`;
+    },
+  },
+});
+</script>
---
+++ test/components/CardLink.spec.ts
@@ -0,0 +1,51 @@
+import { mount } from "@vue/test-utils";
+import CardLink from "@/components/CardLink.vue";
+
+describe("CardLink", () => {
+  it("creates a scryfall link from name", () => {
+    const wrapper = mount(CardLink, {
+      propsData: {
+        name: "Card Name",
+      },
+    });
+
+    expect(wrapper.find("a").attributes("href")).toBe(
+      "https://scryfall.com/search?q=name%3D%22Card%20Name%22"
+    );
+  });
+
+  it("uses single quotes if card name contains double qotes", () => {
+    const wrapper = mount(CardLink, {
+      propsData: {
+        name: 'Card Name with "quotes"',
+      },
+    });
+
+    expect(wrapper.find("a").attributes("href")).toBe(
+      "https://scryfall.com/search?q=name%3D%27Card%20Name%20with%20%22quotes%22%27"
+    );
+  });
+
+  it("defaults target to _blank", () => {
+    expect(
+      mount(CardLink, {
+        propsData: {
+          name: "Card Name",
+        },
+      })
+        .find("a")
+        .attributes("target")
+    ).toBe("_blank");
+
+    expect(
+      mount(CardLink, {
+        propsData: {
+          name: "Card Name",
+          target: "_top",
+        },
+      })
+        .find("a")
+        .attributes("target")
+    ).toBe("_top");
+  });
+});
