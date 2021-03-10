b60e81ee1e34123ba1ab358085ffed0424fdf349
Blade Barringer <blade@crookedneighbor.com>
2021-03-09 21:53:59 -0600

    chore: fixup logo

--- components/Logo.vue
+++ components/Logo.vue
@@ -3,24 +3,13 @@
     <img
       src="~/assets/images/logo.svg"
       class="h-32 inline-block md:h-48 lg:h-56"
+      alt="Commander Spellbook Logo"
+      aria-hidden="true"
     />
     <img
       src="~/assets/images/title.svg"
       class="h-32 inline-block md:h-48 lg:h-56"
+      alt="Commander Spellbook"
     />
   </div>
 </template>
-
-<style scoped>
-.spellbook-logo {
-  background: url("~assets/images/full-logo.svg");
-  @apply m-auto bg-contain bg-center bg-no-repeat w-full;
-}
-
-@media (min-width: 1280px) {
-  .title {
-    /* this is actually text-8xl, but the current verson of Nuxt doesn't support it */
-    font-size: 6rem;
-  }
-}
-</style>
