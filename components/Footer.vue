e0175602986c075252b0342ca20e03cad0cedacb
Blade Barringer <blade@crookedneighbor.com>
2021-03-10 22:16:49 -0600

    refactor: collapse footer link classes

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,19 +1,19 @@
 <template>
   <footer class="footer">
     <div class="container md:flex flex-row text-center md:text-left">
-      <div class="flex-grow flex flex-col">
+      <div class="link-collection">
         <nuxt-link to="/advanced-search">Advanced Search</nuxt-link>
         <nuxt-link to="/syntax-guide">Syntax Guide</nuxt-link>
         <nuxt-link to="/random">Random Combo</nuxt-link>
       </div>
-      <div class="flex-grow flex flex-col">
+      <div class="link-collection">
         <nuxt-link to="/about">About</nuxt-link>
         <nuxt-link to="/metrics">Combo Metrics</nuxt-link>
         <nuxt-link to="/how-to-submit-a-combo">Combo Submission</nuxt-link>
         <nuxt-link to="/fan-site-policy">Fan Site Policy</nuxt-link>
         <nuxt-link to="/privacy-policy">Privacy Policy</nuxt-link>
       </div>
-      <div class="flex-grow flex flex-col">
+      <div class="link-collection">
         <a href="https://www.patreon.com/commanderspellbook" target="_blank"
           >Patreon</a
         >
@@ -46,5 +46,9 @@ export default Vue.extend({
 
 .footer a {
   @apply text-white no-underline;
+}
+
+.link-collection {
+  @apply flex flex-grow flex-col;
 }
 </style>
