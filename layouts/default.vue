beb13bbc566a4a8abdda7ddf58a0f8d1ddadaade
Blade Barringer <blade@crookedneighbor.com>
2021-03-13 10:21:50 -0600

    chore: remove beta mask component
    closes #69

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,26 +1,23 @@
 <template>
   <div>
-    <BetaMask>
-      <a ref="skipLink" href="#main" class="sr-only">Skip to main content</a>
-      <AnalyticsCookieBanner />
-      <div class="bg-dark">
-        <nav class="container">
-          <SearchBar />
-        </nav>
-        <div class="gradient search-bar-border"></div>
-      </div>
-      <main id="main" tabindex="-1" class="pb-16">
-        <Nuxt />
-      </main>
-      <Footer class="mt-24 lg:mt-48 z-0" />
-    </BetaMask>
+    <a ref="skipLink" href="#main" class="sr-only">Skip to main content</a>
+    <AnalyticsCookieBanner />
+    <div class="bg-dark">
+      <nav class="container">
+        <SearchBar />
+      </nav>
+      <div class="gradient search-bar-border"></div>
+    </div>
+    <main id="main" tabindex="-1" class="pb-16">
+      <Nuxt />
+    </main>
+    <Footer class="mt-24 lg:mt-48 z-0" />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import AnalyticsCookieBanner from "@/components/AnalyticsCookieBanner.vue";
-import BetaMask from "@/components/BetaMask.vue";
 import SearchBar from "@/components/SearchBar.vue";
 import Footer from "@/components/Footer.vue";
 
@@ -28,7 +25,6 @@ export default Vue.extend({
   components: {
     AnalyticsCookieBanner,
     SearchBar,
-    BetaMask,
     Footer,
   },
   watch: {
--- layouts/landing.vue
+++ layouts/landing.vue
@@ -1,25 +1,21 @@
 <template>
   <div>
-    <BetaMask>
-      <AnalyticsCookieBanner />
-      <main>
-        <Nuxt />
-      </main>
-      <Footer />
-    </BetaMask>
+    <AnalyticsCookieBanner />
+    <main>
+      <Nuxt />
+    </main>
+    <Footer />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import AnalyticsCookieBanner from "@/components/AnalyticsCookieBanner.vue";
-import BetaMask from "@/components/BetaMask.vue";
 import Footer from "@/components/Footer.vue";
 
 export default Vue.extend({
   components: {
     AnalyticsCookieBanner,
-    BetaMask,
     Footer,
   },
 });
