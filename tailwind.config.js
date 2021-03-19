1ba77cc28cc565c69d5b217a72b4ac32290af3de
Blade Barringer <blade@crookedneighbor.com>
2021-03-19 13:35:18 -0500

    chore: upgrade tailwind v2
    closes #73

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
@@ -11,19 +13,9 @@ module.exports = {
         warning: "#FFC580",
       },
     },
-    fontSize: {
-      // these are opt in breaking changes to be ready for Tailwind v2 when it releases
-      xs: ["0.75rem", { lineHeight: "1rem" }],
-      sm: ["0.875rem", { lineHeight: "1.25rem" }],
-      base: ["1rem", { lineHeight: "1.5rem" }],
-      lg: ["1.125rem", { lineHeight: "1.75rem" }],
-      xl: ["1.25rem", { lineHeight: "1.75rem" }],
-      "2xl": ["1.5rem", { lineHeight: "2rem" }],
-      "3xl": ["1.875rem", { lineHeight: "2.25rem" }],
-      "4xl": ["2.25rem", { lineHeight: "2.5rem" }],
-      "5xl": ["3rem", { lineHeight: "1" }],
-      "6xl": ["4rem", { lineHeight: "1" }],
-    },
   },
-  // ...
+  variants: {
+    extend: {},
+  },
+  plugins: [],
 };
