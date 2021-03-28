fd22a6f5a225b853a307bfe2bf41c47b54561145
Blade Barringer <blade@crookedneighbor.com>
2021-03-28 14:39:28 -0500

    refactor: split out formatting from lookup

--- lib/api/spellbook-api.ts
+++ lib/api/spellbook-api.ts
@@ -1,7 +1,4 @@
-import normalizeDatabaseValue from "./normalize-database-value";
-import CardGrouping from "./models/card-grouping";
-import SpellbookList from "./models/list";
-import ColorIdentity from "./models/color-identity";
+import formatApiResponse from "./format-api-response";
 import type {
   CommanderSpellbookAPIResponse,
   FormattedApiResponse,
@@ -11,55 +8,6 @@ const LOCAL_API_ENDPOINT = "/api/combo-data.json";
 
 let cachedPromise: Promise<FormattedApiResponse[]>;
 let useCachedResponse = false;
-
-function formatApiResponse(
-  apiResponse: CommanderSpellbookAPIResponse
-): FormattedApiResponse[] {
-  return apiResponse.valueRanges[0].values
-    .filter((combo) => {
-      // ensures the spreadsheet has all values needed
-      // in particular, the first card and a color identity
-      return combo.length > 13 && combo[1] && combo[11];
-    })
-    .map((combo) => {
-      return combo.map(normalizeDatabaseValue);
-    })
-    .map((combo) => {
-      const id = combo[0];
-      const cardNames = [
-        combo[1],
-        combo[2],
-        combo[3],
-        combo[4],
-        combo[5],
-        combo[6],
-        combo[7],
-        combo[8],
-        combo[9],
-        combo[10],
-      ].filter((cardName) => cardName);
-
-      const cards = CardGrouping.create(cardNames);
-      const colorIdentity = new ColorIdentity(combo[11]);
-      const prerequisites = SpellbookList.create(combo[12]);
-      const steps = SpellbookList.create(combo[13]);
-      const results = SpellbookList.create(combo[14]);
-      const hasBannedCard = combo[15].toLowerCase() === "true";
-      const hasSpoiledCard = combo[16].toLowerCase() === "true";
-
-      return {
-        commanderSpellbookId: id,
-        permalink: `https://commanderspellbook.com/?id=${id}`,
-        cards,
-        colorIdentity,
-        prerequisites,
-        steps,
-        results,
-        hasBannedCard,
-        hasSpoiledCard,
-      };
-    });
-}
 
 export default function lookupApi(): Promise<FormattedApiResponse[]> {
   if (useCachedResponse) {
