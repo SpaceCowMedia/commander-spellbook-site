d4ea765c59bb624093c2ed8411dbe5a5907eb56b
Blade Barringer <blade@crookedneighbor.com>
2021-03-28 07:31:24 -0500

    refactor: minify google sheets data

--- scripts/copy-google-sheets.js
+++ scripts/copy-google-sheets.js
@@ -1,11 +1,21 @@
 const https = require("https");
 const fs = require("fs");
 
-const file = fs.createWriteStream("./static/combo-data.json");
+https
+  .get(
+    "https://sheets.googleapis.com/v4/spreadsheets/1JJo8MzkpuhfvsaKVFVlOoNymscCt-Aw-1sob2IhpwXY/values:batchGet?ranges=combos!A2:Q&key=AIzaSyDzQ0jCf3teHnUK17ubaLaV6rcWf9ZjG5E",
+    (response) => {
+      let data = "";
 
-https.get(
-  "https://sheets.googleapis.com/v4/spreadsheets/1JJo8MzkpuhfvsaKVFVlOoNymscCt-Aw-1sob2IhpwXY/values:batchGet?ranges=combos!A2:Q&key=AIzaSyDzQ0jCf3teHnUK17ubaLaV6rcWf9ZjG5E",
-  (response) => {
-    response.pipe(file);
-  }
-);
+      response.on("data", (chunk) => {
+        data += chunk;
+      });
+      response.on("end", () => {
+        const minified = JSON.stringify(JSON.parse(data));
+        fs.writeFileSync("./static/combo-data.json", minified);
+      });
+    }
+  )
+  .on("error", (err) => {
+    throw err;
+  });
