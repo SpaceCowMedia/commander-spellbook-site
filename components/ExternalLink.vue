8eb17bce800276d6b500b33ffb42e41111b928ef
Blade Barringer <blade@crookedneighbor.com>
2021-03-31 21:12:44 -0500

    feat: add external link component

---
+++ components/ExternalLink.vue
@@ -0,0 +1,27 @@
+<template>
+  <a
+    :href="to"
+    :target="target"
+    rel="noopener"
+    @click="$emit('click')"
+    @focus="$emit('focus')"
+    ><slot
+  /></a>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  props: {
+    to: {
+      type: String,
+      default: "",
+    },
+    target: {
+      type: String,
+      default: "_blank",
+    },
+  },
+});
+</script>
