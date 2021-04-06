5745fae6b458a2d664c7a22c85e223cb01a16efb
Blade Barringer <blade@crookedneighbor.com>
2021-04-05 22:56:54 -0500

    refactor: pull in autocomplete data in script

---
+++ lib/api/color-autocompletes.ts
@@ -0,0 +1,214 @@
+export default [
+  {
+    value: "colorless",
+    label: "Colorless :manac:",
+  },
+
+  // mono colors
+  {
+    value: "mono white",
+    label: "Mono White :manaw:",
+  },
+  {
+    value: "mono blue",
+    label: "Mono Blue :manau:",
+    alias: /^u$/,
+  },
+  {
+    value: "mono black",
+    label: "Mono Black :manab:",
+  },
+  {
+    value: "mono red",
+    label: "Mono Red :manar:",
+  },
+  {
+    value: "mono green",
+    label: "Mono Green :manag:",
+  },
+
+  // guilds
+  {
+    value: "azorius",
+    label: "Azorius :manaw::manau:",
+    // catch misspellings of azorius
+    alias: /^([wu]{0,2}$|azorio)/,
+  },
+  {
+    value: "dimir",
+    label: "Dimir :manau::manab:",
+    alias: /^[ub]{0,2}$/,
+  },
+  {
+    value: "rakdos",
+    label: "Rakdos :manab::manar:",
+    alias: /^[br]{0,2}$/,
+  },
+  {
+    value: "gruul",
+    label: "Gruul :manar::manag:",
+    alias: /^[rg]{0,2}$/,
+  },
+  {
+    value: "selesnya",
+    label: "Selesnya :manag::manaw:",
+    alias: /^[gw]{0,2}$/,
+  },
+  {
+    value: "orzhov",
+    label: "Orzhov :manaw::manab:",
+    alias: /^[wb]{0,2}$/,
+  },
+  {
+    value: "izzet",
+    label: "Izzet :manau::manar:",
+    alias: /^[ur]{0,2}$/,
+  },
+  {
+    value: "golgari",
+    label: "Golgari :manab::manag:",
+    alias: /^[bg]{0,2}$/,
+  },
+  {
+    value: "boros",
+    label: "Boros :manar::manaw:",
+    alias: /^[rw]{0,2}$/,
+  },
+  {
+    value: "simic",
+    label: "Simic :manag::manau:",
+    alias: /^[gu]{0,2}$/,
+  },
+
+  // shards/wedges
+  {
+    value: "esper",
+    label: "Esper :manaw::manau::manab:",
+    alias: /^[wub]{0,3}$/,
+  },
+  {
+    value: "grixis",
+    label: "Grixis :manau::manab::manar:",
+    alias: /^[ubr]{0,3}$/,
+  },
+  {
+    value: "jund",
+    label: "Jund :manab::manar::manag:",
+    alias: /^[brg]{0,3}$/,
+  },
+  {
+    value: "naya",
+    label: "Naya :manar::manag::manaw:",
+    alias: /^[rgw]{0,3}$/,
+  },
+  {
+    value: "bant",
+    label: "Bant :manag::manaw::manau:",
+    alias: /^[gwu]{0,3}$/,
+  },
+  {
+    value: "abzan",
+    label: "Abzan :manaw::manab::manag:",
+    alias: /^[wbg]{0,3}$/,
+  },
+  {
+    value: "jeskai",
+    label: "Jeskai :manau::manar::manaw:",
+    alias: /^[urw]{0,3}$/,
+  },
+  {
+    value: "sultai",
+    label: "Sultai :manab::manag::manau:",
+    alias: /^[bgu]{0,3}$/,
+  },
+  {
+    value: "mardu",
+    label: "Mardu :manar::manaw::manab:",
+    alias: /^[rwb]{0,3}$/,
+  },
+  {
+    value: "temur",
+    label: "Temur :manag::manau::manar:",
+    alias: /^[gur]{0,3}$/,
+  },
+
+  // 4 color
+  {
+    value: "yoretiller",
+    label: "Yore-Tiller :manaw::manau::manab::manar:",
+    alias: /^[wubr]{0,4}$/,
+  },
+  {
+    value: "glinteye",
+    label: "Glint-Eye :manau::manab::manar::manag:",
+    alias: /^[ubrg]{0,4}$/,
+  },
+  {
+    value: "dunebrood",
+    label: "Dune-Brood :manab::manar::manag::manaw:",
+    alias: /^[brgw]{0,4}$/,
+  },
+  {
+    value: "inktreader",
+    label: "Ink-Treader :manar::manag::manaw::manau:",
+    alias: /^[rgwu]{0,4}$/,
+  },
+  {
+    value: "witchmaw",
+    label: "Witch-Maw :manag::manaw::manau::manab:",
+    alias: /^[gwub]{0,4}$/,
+  },
+
+  // 5 color (comment here mostly for symmetry with the other sections :) )
+  {
+    value: "five color",
+    label: "Five Color :manaw::manau::manab::manar::manag:",
+    alias: /^(5|[wubrg]{0,5})$/,
+  },
+
+  // these are their own entries instaead of
+  // aliases for their 4 color name so that
+  // the 4 color combos don't show up as options
+  // when you type the one color they do not contain
+  // or the alternate name for the 4 color combos
+  {
+    value: "sans white",
+    label: "Sans White :manaw::manau::manab::manar:",
+  },
+  {
+    value: "sans blue",
+    label: "Sans Blue :manab::manar::manag::manaw:",
+  },
+  {
+    value: "sans black",
+    label: "Sans Black :manar::manag::manaw::manau:",
+  },
+  {
+    value: "sans red",
+    label: "Sans Red :manag::manaw::manau::manab:",
+  },
+  {
+    value: "sans green",
+    label: "Sans Green :manaw::manau::manab::manar:",
+  },
+  {
+    value: "chaos",
+    label: "Chaos :manaw::manau::manab::manar:",
+  },
+  {
+    value: "aggression",
+    label: "Aggression :manab::manar::manag::manaw:",
+  },
+  {
+    value: "altruism",
+    label: "Altruism :manar::manag::manaw::manau:",
+  },
+  {
+    value: "growth",
+    label: "Growth :manag::manaw::manau::manab:",
+  },
+  {
+    value: "artifice",
+    label: "Artifice :manaw::manau::manab::manar:",
+  },
+];
--- scripts/copy-google-sheets/index.ts
+++ scripts/copy-google-sheets/index.ts
@@ -1,13 +1,32 @@
 import fs from "fs";
+import formatApiResponse from "../../lib/api/format-api-response";
 import getData from "../shared/get";
 import log from "../shared/log";
+import { collectCardNames, collectResults } from "./collect-autocomplete";
+
+import type {
+  CommanderSpellbookAPIResponse,
+  FormattedApiResponse,
+} from "../../lib/api/types";
 
 log("Fetching Combo data from Google Sheets");
 
 getData(
   "https://sheets.googleapis.com/v4/spreadsheets/1JJo8MzkpuhfvsaKVFVlOoNymscCt-Aw-1sob2IhpwXY/values:batchGet?ranges=combos!A2:Q&key=AIzaSyDzQ0jCf3teHnUK17ubaLaV6rcWf9ZjG5E"
-).then((data) => {
-  const minified = JSON.stringify(data);
-  fs.writeFileSync("./static/api/combo-data.json", minified);
+).then((rawData) => {
+  const combos = formatApiResponse(rawData as CommanderSpellbookAPIResponse);
+  const cardNames = collectCardNames(combos);
+  const results = collectResults(combos);
+
+  log("Writing /static/api/combo-data.json");
+  fs.writeFileSync("./static/api/combo-data.json", JSON.stringify(rawData));
   log("/static/api/combo-data.json written", "green");
+
+  log("Writing /static/api/card-names.json");
+  fs.writeFileSync("./static/api/card-names.json", JSON.stringify(cardNames));
+  log("/static/api/card-names.json written", "green");
+
+  log("Writing /static/api/results.json");
+  fs.writeFileSync("./static/api/results.json", JSON.stringify(results));
+  log("/static/api/results.json written", "green");
 });
