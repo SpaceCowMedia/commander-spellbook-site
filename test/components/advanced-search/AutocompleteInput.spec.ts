ac015a4e785003385b2797f5aef724dec0f8725e
Blade Barringer <blade@crookedneighbor.com>
2021-04-10 10:43:33 -0500

    chore: fixup linting error

--- test/components/advanced-search/AutocompleteInput.spec.ts
+++ test/components/advanced-search/AutocompleteInput.spec.ts
@@ -153,7 +153,6 @@ describe("AutocompleteInput", () => {
         autocompleteOptions: options,
       },
     });
-    const vm = wrapper.vm as VueComponent;
 
     await wrapper.setData({
       matchingAutocompleteOptions: options,
