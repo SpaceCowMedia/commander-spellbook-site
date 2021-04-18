7b2d13ed6aba7fa0ab792378288fae1accbaeec0
Blade Barringer <blade@crookedneighbor.com>
2021-04-18 14:56:55 -0500

    feat: provide way to use google sheets api via findById api module

--- lib/api/find-by-id.ts
+++ lib/api/find-by-id.ts
@@ -3,11 +3,12 @@ import lookupApi from "./spellbook-api";
 import type { FormattedApiResponse } from "./types";
 
 export default async function findById(
-  id: string | number
+  id: string | number,
+  useGoogleSheetsEndpoint = false
 ): Promise<FormattedApiResponse> {
   id = String(id);
 
-  const combos = await lookupApi();
+  const combos = await lookupApi(useGoogleSheetsEndpoint);
   const combo = combos.find((c) => c.commanderSpellbookId === id);
 
   if (!combo) {
