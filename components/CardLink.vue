6f7f093bfbd5914cefe7d51bb991a3f82b3e74e0
Blade Barringer <blade@crookedneighbor.com>
2021-04-26 13:25:53 -0500

    feat: set default card link to EDHRec

--- components/CardLink.vue
+++ components/CardLink.vue
@@ -5,6 +5,7 @@
 <script lang="ts">
 import Vue from "vue";
 import ExternalLink from "@/components/ExternalLink.vue";
+import Card from "@/lib/api/models/card";
 
 export default Vue.extend({
   components: {
@@ -16,8 +17,19 @@ export default Vue.extend({
       default: "",
     },
   },
+  data() {
+    return {
+      card: new Card(this.name),
+    };
+  },
   computed: {
     link(): string {
+      const edhrecLink = this.card.getEdhrecLink();
+
+      if (edhrecLink) {
+        return edhrecLink;
+      }
+
       let quotes = "%22";
 
       if (this.name.includes('"')) {
