754a2de1f4cdc6219afcd13ba8108aff30a02abe
Blade Barringer <blade@crookedneighbor.com>
2021-06-30 19:52:07 -0500

    feat: update featured page to use AFR combos (#170)
    * feat: update featured page to use AFR combos
    
    * refactor: shorten featured combos button text

--- pages/index.vue
+++ pages/index.vue
@@ -24,7 +24,7 @@
           Random Combo
         </RandomButton>
         <nuxt-link to="/featured/" class="previwed-combos-button button md:m-1">
-          MH2 Combos
+          AFR Combos
         </nuxt-link>
       </div>
 
