e54a693a04b782c0cfd406c99890e7222bfd603b
Blade Barringer <blade@crookedneighbor.com>
2021-07-21 14:29:45 -0500

    fix: update featured page to include afc cards

--- scripts/download-data/is-featured.ts
+++ scripts/download-data/is-featured.ts
@@ -9,5 +9,5 @@ export default function isFeatured(sfData: ScryfallEntry): boolean {
 
   // update this logic for whatever set codes we
   // want to feature on the site
-  return setCode === "afr";
+  return setCode === "afr" || setCode === "afc";
 }
