0d8ad8dea3746eb16f2c79ceee9c31d4fb54140b
Blade Barringer <blade@crookedneighbor.com>
2021-09-09 22:00:48 -0500

    refactor: simplify login logic

--- pages/login.vue
+++ pages/login.vue
@@ -1,70 +1,25 @@
 <template>
-  <div>
-    <div v-if="!linkSent">
-      <div class="mb-2 text-center">
-        <p>Enter your email address to log in</p>
-      </div>
+  <EmailAuth
+    prompt-text="Enter your email address to log in"
+    submit-button-text="Continue"
+    link-sent-text="Check your email on this device for a sign in link."
+  >
+    <hr class="my-2" />
 
-      <form @submit.prevent="userSignIn">
-        <div>
-          <label class="sr-only" for="email">Email</label>
-          <input
-            id="email"
-            v-model="email"
-            class="input"
-            :class="{ 'border border-red-400': error }"
-            type="text"
-            @input="error = ''"
-            placeholder="email address"
-          />
-          <ErrorMessage :message="error" />
-        </div>
-
-        <button type="submit" class="button w-full">Continue</button>
-      </form>
-
-      <hr class="my-2" />
-
-      <nuxt-link class="button dark w-full text-center" to="/sign-up/"
-        >Create an Account</nuxt-link
-      >
-    </div>
-
-    <div v-else>Check your email on this device for a sign in link.</div>
-  </div>
+    <nuxt-link class="button dark w-full text-center" to="/sign-up/"
+      >Create an Account</nuxt-link
+    >
+  </EmailAuth>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import ErrorMessage from "@/components/ErrorMessage.vue";
+import EmailAuth from "@/components/auth/Email.vue";
 
 export default Vue.extend({
   components: {
-    ErrorMessage,
+    EmailAuth,
   },
   layout: "splash",
-  data() {
-    return {
-      error: "",
-      email: "",
-      linkSent: false,
-    };
-  },
-  methods: {
-    userSignIn() {
-      this.error = "";
-
-      return this.$store
-        .dispatch("auth/requestMagicLink", {
-          email: this.email,
-        })
-        .then(() => {
-          this.linkSent = true;
-        })
-        .catch((err) => {
-          this.error = err.message;
-        });
-    },
-  },
 });
 </script>
--- pages/sign-up.vue
+++ pages/sign-up.vue
@@ -1,82 +1,19 @@
 <template>
-  <div class="static-page">
-    <ArtCircle
-      card-name="Codie, Vociferous Codex"
-      artist="Daniel Ljunggren"
-      class="m-auto md:block hidden"
-    />
-    <h1 class="heading-title">Sign up</h1>
-
-    <div>
-      <form @submit.prevent="userSignUp">
-        <ErrorMessage v-if="error" :message="error" @close="error = ''" />
-
-        <div class="mb-2">
-          <label class="sr-only" for="email">Email</label>
-          <input
-            id="email"
-            v-model="email"
-            class="input"
-            type="text"
-            placeholder="email address"
-          />
-        </div>
-
-        <div class="mb-2">
-          <label class="sr-only" for="password">Password</label>
-          <input
-            id="password"
-            v-model="password"
-            class="input"
-            type="password"
-            placeholder="password"
-          />
-        </div>
-
-        <button type="submit" class="button w-full">Sign Up</button>
-
-        <hr class="my-2" />
-
-        <p class="text-center">
-          Already have an account? <nuxt-link to="/login/">Login</nuxt-link>
-        </p>
-      </form>
-    </div>
-  </div>
+  <EmailAuth
+    prompt-text="Enter your email address to create an account"
+    submit-button-text="Create Account"
+    link-sent-text="Check your email to complete the setup process."
+  />
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import ArtCircle from "@/components/ArtCircle.vue";
+import EmailAuth from "@/components/auth/Email.vue";
 
 export default Vue.extend({
   components: {
-    ArtCircle,
+    EmailAuth,
   },
   layout: "splash",
-  data() {
-    return {
-      error: "",
-      email: "",
-      password: "",
-    };
-  },
-  methods: {
-    userSignUp() {
-      this.error = "";
-
-      return this.$store
-        .dispatch("auth/signUp", {
-          email: this.email,
-          password: this.password,
-        })
-        .then(() => {
-          this.$router.push("/profile/");
-        })
-        .catch((err) => {
-          this.error = err.message;
-        });
-    },
-  },
 });
 </script>
