1ca2e1c35714c76cd53366e32739aca4e15fb3c7
Blade Barringer <blade@crookedneighbor.com>
2021-10-24 11:24:56 -0500

    feat: add Dashboard Section component

---
+++ components/dashboard/Section.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="border-gray-400 border rounded-sm">
+    <h2 class="p-4 border-b border-gray-400 bg-gray-100">{{ title }}</h2>
+    <div class="p-4">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  props: {
+    title: {
+      type: String,
+      default: "",
+    },
+  },
+});
+</script>
