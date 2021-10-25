4da95ff62617089b873eb3ee4acacb4a185d4b12
Blade Barringer <blade@crookedneighbor.com>
2021-10-24 22:03:40 -0500

    feat: create profile input component

---
+++ components/dashboard/ProfileInput.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="my-4">
+    <label>{{ label }}</label>
+    <div>
+      <input v-model="localState" type="text" class="input" />
+    </div>
+    <span class="helper-text">{{ helperText }}</span>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  props: {
+    label: {
+      type: String,
+      default: "",
+    },
+    helperText: {
+      type: String,
+      default: "",
+    },
+    value: {
+      type: String,
+      default: "",
+    },
+  },
+  computed: {
+    localState: {
+      get(): string {
+        return this.value;
+      },
+      set(localState: string): void {
+        this.$emit("input", localState);
+      },
+    },
+  },
+});
+</script>
---
+++ test/components/dashboard/ProfileInput.test.ts
@@ -0,0 +1,22 @@
+import { shallowMount } from "@vue/test-utils";
+import ProfileInput from "@/components/dashboard/ProfileInput.vue";
+
+describe("ProfileInput", () => {
+  it("configures title", () => {
+    const wrapper = shallowMount(ProfileInput, {
+      propsData: {
+        label: "A Label",
+        value: "some value",
+        helperText: "some text describing the input",
+      },
+    });
+
+    expect(wrapper.find("label").text()).toBe("A Label");
+    expect(wrapper.find(".helper-text").text()).toBe(
+      "some text describing the input"
+    );
+    expect((wrapper.find("input").element as HTMLInputElement).value).toBe(
+      "some value"
+    );
+  });
+});
