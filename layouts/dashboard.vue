d30f0c05f60fd00cd922d7627152e54ae5d11299
Blade Barringer <blade@crookedneighbor.com>
2021-12-26 13:16:48 -0600

    feat: toggle dashboard based on provisioning

--- layouts/dashboard.vue
+++ layouts/dashboard.vue
@@ -1,27 +1,38 @@
 <template>
-  <div class="md:flex md:h-screen">
-    <DashboardNav />
-    <main class="p-8 flex-grow w-2/3 max-w-5xl mx-auto">
-      <Nuxt v-if="loaded" />
-      <div class="flex h-full justify-center pt-32" v-else>
-        <div class="spinner h-32 w-32 bg-gray-300"></div>
-      </div>
-    </main>
+  <div>
+    <div v-if="provisioned" class="w-full md:flex md:h-screen">
+      <DashboardNav />
+      <main class="p-8 flex-grow md:w-2/3 max-w-5xl mx-auto">
+        <Nuxt />
+      </main>
+    </div>
+    <div v-else>
+      <CompleteAccountSetup class="md:h-screen" />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import DashboardNav from "@/components/dashboard/Nav.vue";
+import CompleteAccountSetup from "@/components/dashboard/CompleteAccountSetup.vue";
 
 export default Vue.extend({
   components: {
     DashboardNav,
+    CompleteAccountSetup,
   },
   computed: {
-    loaded(): boolean {
+    provisioned(): boolean {
       return this.$store.getters["auth/user"].provisioned === true;
     },
+  },
+  async mounted() {
+    await this.$store.dispatch("auth/lookupUser");
+
+    if (!this.$store.getters["auth/isAuthenticated"]) {
+      this.$router.push("/");
+    }
   },
 });
 </script>
