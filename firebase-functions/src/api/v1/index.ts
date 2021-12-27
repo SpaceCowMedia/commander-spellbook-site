9d016ed78d2c14a75d432a4ab855e58e7615ce32
Blade Barringer <blade@crookedneighbor.com>
2021-12-26 22:03:53 -0600

    fix: correct 404 handling

--- firebase-functions/src/api/v1/index.ts
+++ firebase-functions/src/api/v1/index.ts
@@ -10,7 +10,7 @@ app.use(cors({ origin: true }));
 app.use(bodyParser.json());
 app.use(requireAuthentication);
 app.use("/user", user);
-app.use("*", (_, res) => {
+app.use((_, res) => {
   // TODO anything else we need to do here?
   res.status(404).json({
     success: false,
