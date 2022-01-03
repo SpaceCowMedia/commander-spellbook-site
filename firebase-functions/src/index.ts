27e83812be174562c8a777f2b0560f446e9213ab
Blade Barringer <blade@crookedneighbor.com>
2022-01-03 10:14:43 -0600

    chore: ignore eslint rule inconsistent between local and ci

--- firebase-functions/src/index.ts
+++ firebase-functions/src/index.ts
@@ -1,6 +1,14 @@
 import admin from "firebase-admin";
-import apiApp from "./api";
 import { https } from "firebase-functions";
+// I don't know why, but the order of apiApp's
+// import errors on CI, but not locally.
+// If I correct it for CI, it errors locally.
+// is the only solution to ignore it entirely?
+// Setting it to the correct location for CI,
+// so removing the disable comment should
+// make it fail locally and pass on CI
+// eslint-disable-next-line import/order
+import apiApp from "./api";
 
 admin.initializeApp();
 
