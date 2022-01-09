6fe540ce5150effac45f91fb39ed06411edac292
Blade Barringer <blade@crookedneighbor.com>
2022-01-09 15:37:00 -0600

    feat: add site-settings route to api

--- firebase-functions/src/api/v1/index.ts
+++ firebase-functions/src/api/v1/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import requireAuthentication from "./middleware/require-authentication";
 import user from "./routes/user";
+import siteSettings from "./routes/site";
 
 const router = Router();
 
 router.use(requireAuthentication);
 router.use("/user", user);
+router.use("/site-settings", siteSettings);
 
 export default router;
