4ff1c6df0e1c61f75da15c0fe2516735eca8ca47
Blade Barringer <blade@crookedneighbor.com>
2023-02-23 08:58:42 -0600

    chore: try

--- scripts/check-changelog.ts
+++ scripts/check-changelog.ts
@@ -1,19 +1,30 @@
 import fs from "fs";
 
-let result = "SHOULD_DEPLOY";
+let result = "SHOULD_NOT_DEPLOY";
+const GITHUB_OUTPUT_LOCATION = process.env.GITHUB_OUTPUT;
+
+if (!GITHUB_OUTPUT_LOCATION) {
+  throw new Error(
+    "Could not find the GITHUB_OUTPUT file. Not in the Github Actions Environment. Abort!"
+  );
+}
+
+const GITHUB_OUTPUT = fs.readFileSync(GITHUB_OUTPUT_LOCATION, "utf8");
 
 const changelog = JSON.parse(
   fs.readFileSync("./frontend/static/changelog.json", "utf8")
 );
 
 if (
-  changelog.addedCombos.length === 0 &&
-  changelog.deletedCombos.length === 0 &&
-  changelog.updatedCombos.length === 0
+  changelog.addedCombos.length > 0 ||
+  changelog.deletedCombos.length > 0 ||
+  changelog.updatedCombos.length > 0
 ) {
-  result = "NO_COMBO_CHANGES";
+  result = "COMBO_DATA_CHANGED";
 }
 
-console.log(process.env.GITHUB_OUTPUT);
-
-fs.writeFileSync("./deploy-result", `result=${result}`);
+fs.writeFileSync(
+  GITHUB_OUTPUT_LOCATION,
+  `${GITHUB_OUTPUT}
+result=${result}`
+);
