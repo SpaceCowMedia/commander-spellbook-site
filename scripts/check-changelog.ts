9ea6195f896dd04fd753c9ea68c577ac7ee7bf45
Blade Barringer <blade@crookedneighbor.com>
2023-02-24 21:55:54 -0600

    refactor: improve output variable checking

--- scripts/check-changelog.ts
+++ scripts/check-changelog.ts
@@ -1,7 +1,7 @@
 import fs from "fs";
 import saveGithubOutput from "./shared/save-github-output";
 
-let result = "SHOULD_NOT_DEPLOY";
+let shouldDeploy = "NO";
 
 const changelog = JSON.parse(
   fs.readFileSync("./frontend/static/changelog.json", "utf8")
@@ -12,7 +12,7 @@ if (
   changelog.deletedCombos.length > 0 ||
   changelog.updatedCombos.length > 0
 ) {
-  result = "COMBO_DATA_CHANGED";
+  shouldDeploy = "YES";
 }
 
-saveGithubOutput("result", result);
+saveGithubOutput("should_deploy", shouldDeploy);
