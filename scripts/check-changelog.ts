8964018816b94e8663cafcf10ed3d79c46072ccc
Blade Barringer <blade@crookedneighbor.com>
2023-02-24 22:05:06 -0600

    feat: include deploy reason

--- scripts/check-changelog.ts
+++ scripts/check-changelog.ts
@@ -2,17 +2,31 @@ import fs from "fs";
 import saveGithubOutput from "./shared/save-github-output";
 
 let shouldDeploy = "NO";
+let deployReasonMessage = "";
 
 const changelog = JSON.parse(
   fs.readFileSync("./frontend/static/changelog.json", "utf8")
 );
 
-if (
-  changelog.addedCombos.length > 0 ||
-  changelog.deletedCombos.length > 0 ||
-  changelog.updatedCombos.length > 0
-) {
+const numOfAddedCombos = changelog.addedCombos.length;
+const numOfDeletedCombos = changelog.deletedCombos.length;
+const numOfUpdatedCombos = changelog.updatedCombos.length;
+
+console.log(changelog);
+
+if (numOfAddedCombos > 0 || numOfDeletedCombos > 0 || numOfUpdatedCombos > 0) {
   shouldDeploy = "YES";
+
+  if (numOfAddedCombos > 0) {
+    deployReasonMessage += ` ${numOfAddedCombos} combo(s) added to the database.`;
+  }
+  if (numOfDeletedCombos > 0) {
+    deployReasonMessage += ` ${numOfDeletedCombos} combo(s) removed from the database.`;
+  }
+  if (numOfUpdatedCombos > 0) {
+    deployReasonMessage += ` ${numOfUpdatedCombos} combo(s) modified in the database.`;
+  }
 }
 
 saveGithubOutput("should_deploy", shouldDeploy);
+saveGithubOutput("deploy_reason", deployReasonMessage);
