2ea733e01c6bee6ca97e2e13da8554c2ef6ebe9e
Blade Barringer <blade@crookedneighbor.com>
2023-03-11 23:11:46 -0600

    fix: remove CSSRuleObject type

--- frontend/components/layout/PlaceholderText/PlaceholderText.tsx
+++ frontend/components/layout/PlaceholderText/PlaceholderText.tsx
@@ -1,5 +1,4 @@
 import styles from "./placeholderText.module.scss";
-import { CSSRuleObject } from "tailwindcss/types/config";
 
 type Props = {
   maxLength?: number;
@@ -9,7 +8,7 @@ type Props = {
 const PlaceholderText = ({ maxLength = 90, minLength = 10 }: Props) => {
   const length = Math.floor(Math.random() * maxLength) + minLength;
 
-  const style: CSSRuleObject = { width: `${length}%` };
+  const style = { width: `${length}%` };
 
   return (
     <span className={styles.placeholderText}>
