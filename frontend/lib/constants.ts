471653a141563d7f3e137c39b7c2818118a67b8d
Blade Barringer <blade@crookedneighbor.com>
2023-03-11 22:21:41 -0600

    feat: apply commits from lib that occurred in main
    * chore: run prettier
    
    * fix: apply size filter fix
    
    applies https://github.com/EDHREC/commander-spellbook-site/commit/7ca503844f07b75472121d501cc19e35dc2efdef
    
    * chore: remove unneeded permissions code
    
    * chore: apply commit for removing isAuthenitcated state in search
    
    applies 3b13a9d2a685ca572201198f5eab7b426ec68258
    
    * fix: correct casing for property in combo button
    
    * fix: remove git merge conflict marker

--- frontend/lib/constants.ts
+++ frontend/lib/constants.ts
@@ -1,14 +1,3 @@
 export const DEFAULT_ORDER = "auto";
 export const DEFAULT_SORT = "popularity";
 export const DEFAULT_VENDOR = "cardkingdom";
-
-// TODO shouldn't the child workspaces be able to reference the peer workspaces?
-// I think it has something to do with the way nuxt is resolving the dependencies
-// export { PERMISSIONS } from "@spellbook/backend/src/shared/constants";
-export const PERMISSIONS = {
-  provisioned: "r",
-  proposeCombo: "p",
-  manageUsers: "m",
-  manageSiteContent: "s",
-  viewUsers: "u",
-};
