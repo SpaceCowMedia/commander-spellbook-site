e55a15c45e731b86f0caddb591ee26482b1b9916
Blade Barringer <blade@crookedneighbor.com>
2023-03-13 21:22:40 -0500

    fix: reinstate non-minification

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -5,7 +5,15 @@ const nextConfig = {
   trailingSlash: true,
   images: {
     unoptimized: true
-  }
+  },
+  webpack(webpackConfig) {
+    return {
+      ...webpackConfig,
+      optimization: {
+        minimize: false, // SSG for combo pages fails to accept routes after the first if the code is minified - only went built (not dev server)
+      },
+    };
+  },
 };
 
 module.exports = nextConfig;
