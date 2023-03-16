91fcab1566de7e9b3d3bbc09cbeb8a381b187fac
accassid <accassid@buffalo.edu>
2023-03-15 21:48:23 -0600

    chore: remove last bit of uncompressed code

--- frontend/lib/spellbook-api.ts
+++ frontend/lib/spellbook-api.ts
@@ -7,8 +7,6 @@ const GOOGLE_SHEETS_API_ENDPOINT =
 const LOCAL_BACKUP_API_ENDPOINT = "/api/combo-data.json";
 
 let cachedPromise: Promise<FormattedApiResponse[]>;
-let compressedCachedPromise: Promise<CompressedApiResponse[]>;
-let useCompressedCachedResponse = false;
 let useCachedResponse = false;
 
 function fetchFromGoogleSheets(): Promise<FormattedApiResponse[]> {
@@ -54,47 +52,6 @@ export default function lookupApi(
   return cachedPromise;
 }
 
-function fetchFromGoogleSheetsCompressed(): Promise<CompressedApiResponse[]> {
-  return fetch(GOOGLE_SHEETS_API_ENDPOINT)
-    .then((res) => res.json())
-    .then(transformGoogleSheetsData);
-}
-
-export function lookupApiCompressed(
-  useGoogleSheetsEndpoint = false
-): Promise<CompressedApiResponse[]> {
-  if (useGoogleSheetsEndpoint) {
-    return fetchFromGoogleSheetsCompressed();
-  }
-
-  if (useCompressedCachedResponse) {
-    if (
-      typeof window === "undefined" ||
-      process.env.NODE_ENV !== "production"
-    ) {
-      return compressedCachedPromise;
-    }
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        resolve(compressedCachedPromise);
-      }, 1000);
-    });
-  }
-
-  if (typeof window === "undefined") {
-    compressedCachedPromise = fetchFromGoogleSheetsCompressed();
-  } else {
-    compressedCachedPromise = fetch(
-      LOCAL_BACKUP_API_ENDPOINT + "?cache-bust=" + Math.random()
-    ).then((res) => res.json());
-  }
-
-  useCompressedCachedResponse = true;
-
-  return compressedCachedPromise;
-}
-
 export function resetCache(): void {
   useCachedResponse = false;
-  useCompressedCachedResponse = false;
 }
