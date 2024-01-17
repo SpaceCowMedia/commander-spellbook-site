b07a1ff91a5c3b9bead9f559791a711bf8478c7e
accassid <accassid@buffalo.edu>
2024-01-17 16:41:51 -0700

    chore: cleaning commit

--- next.config.js
+++ next.config.js
@@ -2,26 +2,6 @@
 const isLocalServer = process.env.NODE_ENV === 'development'
 const beta = process.env.BUILD_TYPE === 'beta' ? 'beta' : ''
 
-
-const OPEN_CORS_HEADERS = [
-  // Allow for specific domains to have access or * for all
-  {
-    key: "Access-Control-Allow-Origin",
-    value: "*",
-    // DOES NOT WORK
-    // value: process.env.ALLOWED_ORIGIN,
-  },
-  // Allows for specific methods accepted
-  {
-    key: "Access-Control-Allow-Methods",
-    value: "GET, POST, PUT, DELETE, OPTIONS",
-  },
-  // Allows for specific headers accepted (These are a few standard ones)
-  {
-    key: "Access-Control-Allow-Headers",
-    value: "Content-Type, Authorization, Origin",
-  },
-]
 const nextConfig = {
   output: "standalone",
   reactStrictMode: true,
@@ -39,20 +19,6 @@ const nextConfig = {
       },
     };
   },
-  // async headers() {
-  //   return [
-  //     {
-  //       source: "/embed.js",
-  //       headers: OPEN_CORS_HEADERS,
-  //    },
-  //     {
-  //       source: "/combo/859-2569-4289-4452/embed",
-  //       headers: OPEN_CORS_HEADERS,
-  //     }
-  //
-  //   ]
-  // }
-
 };
 
 module.exports = nextConfig;
