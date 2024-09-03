250a0b288fdc44ebae0ebbc5192c189de8a40e4d
ldeluigi <44567586+ldeluigi@users.noreply.github.com>
2024-09-03 14:16:16 +0200

    chore: refactor project configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,26 @@ module.exports = {
   env: {
     browser: true,
     node: true,
+    es2020: true,
   },
-  extends: ["next/core-web-vitals", "prettier"],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'next/core-web-vitals',
+  ],
+  plugins: ['react', 'react-hooks', 'prettier'],
   // add your custom rules here
   rules: {
-    "react/no-unescaped-entities": "off",
-    "react-hooks/exhaustive-deps": "off",
-    "@next/next/no-img-element": "off",
+    'react/no-deprecated': 'warn', // Warn about deprecated lifecycle methods
+    'react/no-direct-mutation-state': 'error', // Prevent direct state mutation
+    'react/no-unescaped-entities': 'off',
+    'react-hooks/exhaustive-deps': 'off',
+    '@next/next/no-img-element': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
   },
 };
