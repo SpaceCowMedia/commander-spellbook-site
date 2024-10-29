318a41c426b8a9e5498dabda7057278df79939ba
Luca Deluigi <lucadelu97@gmail.com>
2024-10-29 13:59:24 +0100

    feat: improve button style and fix behaviour

--- src/components/layout/CardImage/CardImage.tsx
+++ src/components/layout/CardImage/CardImage.tsx
@@ -19,11 +19,12 @@ const CardImage: React.FC<Props> = ({ img, name, className }: Props) => {
   const [hasBack, setHasBack] = useState(false);
   const [loaded, setLoaded] = useState(false);
   const [readyToFlip, setReadyToFlip] = useState(false);
-  const [backFacing, setBackFacing] = useState(true);
+  const [backFacing, setBackFacing] = useState(!img.includes(encodeURIComponent(name)));
 
   const flip = () => {
     setBackFacing((prev) => !prev);
   };
+
   const clickFlip = (e: any) => {
     e.currentTarget.blur();
     flip();
@@ -38,17 +39,13 @@ const CardImage: React.FC<Props> = ({ img, name, className }: Props) => {
   }, []);
 
   useEffect(() => {
-    if (backImageRef.current && backImageRef.current.complete) {
+    if (backImageRef.current && backImageRef.current.complete && backImageSrc != cardBack.src) {
       //If there is no back image then set to cardBack.src
       if (backImageRef.current.naturalHeight == 0) {
         setBackImageSrc(cardBack.src);
       } else {
         setHasBack(true);
       }
-    }
-    //determine which side to show
-    if (img.includes(encodeURIComponent(name))) {
-      setBackFacing(false);
     }
   }, [imgBack]);
 
@@ -66,9 +63,14 @@ const CardImage: React.FC<Props> = ({ img, name, className }: Props) => {
         />
       </CardLink>
       {hasBack && (
-        <button id="flip-{name}" className="button cardImage_{name} md:flex" onClick={(e) => clickFlip(e)}>
+        <button
+          id={`flip-${name}`}
+          className={`cardImage_${name} md:flex ${styles.flipperButton}`}
+          onClick={(e) => clickFlip(e)}
+          title="Flip card"
+        >
           <svg
-            height="24"
+            height="30"
             focusable="false"
             aria-hidden="true"
             xmlns="http://www.w3.org/2000/svg"
@@ -77,7 +79,6 @@ const CardImage: React.FC<Props> = ({ img, name, className }: Props) => {
             <path d="M884.3,357.6c116.8,117.7,151.7,277-362.2,320V496.4L243.2,763.8L522,1031.3V860.8C828.8,839.4,1244.9,604.5,884.3,357.6z"></path>
             <path d="M557.8,288.2v138.4l230.8-213.4L557.8,0v142.8c-309.2,15.6-792.1,253.6-426.5,503.8C13.6,527.9,30,330.1,557.8,288.2z"></path>
           </svg>
-          Turn Over
         </button>
       )}
     </div>
