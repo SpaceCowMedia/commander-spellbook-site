fc5f1364964d9e58f3e6200712f3ccad85f40e33
antmoute <lejugeti@gmail.com>
2025-06-30 21:15:33 +0200

    feat: create CombosExportService which converts a list of Variant into a string

---
+++ src/services/combos-export.service.ts
@@ -0,0 +1,61 @@
+import { Variant } from '@space-cow-media/spellbook-client';
+import { getPrerequisiteList } from 'lib/prerequisitesProcessor';
+
+const LINE_BREAK = '\n';
+
+function getIdentity(combo: Variant): string {
+  let identity = '';
+
+  for (let color of combo.identity.split('')) {
+    identity += '{' + color + '}';
+  }
+
+  return identity;
+}
+
+function exportToText(combos: Variant[]): string {
+  if (!combos) {
+    return '';
+  }
+
+  const lines: string[] = [];
+
+  for (let combo of combos) {
+    if (!combo) {
+      continue;
+    }
+
+    lines.push('--------------------------------------');
+    lines.push(getIdentity(combo));
+    lines.push(LINE_BREAK);
+
+    lines.push('Cards Required:');
+    for (let comboCard of combo.uses) {
+      lines.push(`- ${comboCard.card.name}`);
+    }
+    lines.push(LINE_BREAK);
+
+    lines.push('Prerequisites:');
+    const prerequisites = getPrerequisiteList(combo);
+    for (let prereq of prerequisites) {
+      lines.push(`- ${prereq.description}`);
+    }
+    lines.push(LINE_BREAK);
+
+    lines.push('Steps:');
+    const steps = combo.description.split(LINE_BREAK);
+
+    for (let stepIndex = 0; stepIndex < steps.length; stepIndex++) {
+      lines.push(`${stepIndex + 1}. ${steps[stepIndex]}`);
+    }
+    lines.push(LINE_BREAK);
+  }
+
+  return lines.join(LINE_BREAK);
+}
+
+const CombosExportService = {
+  exportToText,
+};
+
+export default CombosExportService;
